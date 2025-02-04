1f66bb8b1cfa9d4e962aa4ac569d092f064a98e9
Lucas Martins <luucasmartins@hotmail.com>
2025-02-04 20:23:49 -0300

    updated script database

--- postgres/init.sql
+++ postgres/init.sql
@@ -3,23 +3,25 @@ START TRANSACTION;
 CREATE TABLE Doctors (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
+    crm VARCHAR(100) NOT NULL,
     specialty VARCHAR(100) NOT NULL,
-    contact_info VARCHAR(100)
+    value_per_appointment DECIMAL(10, 2) NOT NULL,
+    password VARCHAR(100)
 );
 
 CREATE TABLE Patients (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
-    birth_date DATE NOT NULL,
-    contact_info VARCHAR(100)
+    cpf VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(100)
 );
 
 CREATE TABLE AppointmentSlots (
     id SERIAL PRIMARY KEY,
     doctor_id INT NOT NULL,
     available_date DATE NOT NULL,
-    start_time TIME NOT NULL,
-    end_time TIME NOT NULL,
+    available_time TIME NOT NULL,
     FOREIGN KEY (doctor_id) REFERENCES Doctors(id)
 );
 
@@ -27,7 +29,10 @@ CREATE TABLE Appointments (
     id SERIAL PRIMARY KEY,
     slot_id INT NOT NULL,
     patient_id INT NOT NULL,
+    doctor_id INT NOT NULL,
     status_id INT NOT NULL,
+    justification VARCHAR(100) NULL,
+    FOREIGN KEY (doctor_id) REFERENCES Doctors(id),
     FOREIGN KEY (slot_id) REFERENCES AppointmentSlots(id),
     FOREIGN KEY (patient_id) REFERENCES Patients(id)
 );
