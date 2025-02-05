a6beea236720c09dc547b8b7aa7126dbea7d0bce
Edgard Domingues <degased@hotmail.com>
2025-02-05 19:36:27 -0300

    fix env swagger

--- src/Fiap.Hackathon.AppointmentScheduler.Api/Program.cs
+++ src/Fiap.Hackathon.AppointmentScheduler.Api/Program.cs
@@ -48,10 +48,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    
 }
 
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
