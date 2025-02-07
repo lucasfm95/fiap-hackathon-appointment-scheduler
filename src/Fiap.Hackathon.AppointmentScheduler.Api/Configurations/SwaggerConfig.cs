6f43e4d84a4ba168a22b336b4553dd9553bb1dfd
Lucas Martins <luucasmartins@hotmail.com>
2025-02-07 20:05:20 -0300

    updated swagger name of project

--- src/Fiap.Hackathon.AppointmentScheduler.Api/Configurations/SwaggerConfig.cs
+++ src/Fiap.Hackathon.AppointmentScheduler.Api/Configurations/SwaggerConfig.cs
@@ -8,7 +8,7 @@ internal static class SwaggerConfig
     {
         services.AddSwaggerGen(c =>
         {
-            c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "FIAP Hackathon Appointment Scheduler", Version = "v1" });
 
     
             c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
