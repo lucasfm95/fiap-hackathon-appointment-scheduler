5092686d09fd7ce7427c53cf9e23f83e933a9782
Allan Santos <allan.santos@tinnova.com.br>
2025-02-06 21:54:16 -0300

    adjusts

--- src/Fiap.Hackathon.AppointmentScheduler.Infrastructure/Migrations/20250207004938_ApplyPendingChanges.Designer.cs
+++ src/Fiap.Hackathon.AppointmentScheduler.Infrastructure/Migrations/20250207004938_ApplyPendingChanges.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Fiap.Hackathon.AppointmentScheduler.Infrastructure.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Fiap.Hackathon.AppointmentScheduler.Infrastructure.Migrations
 {
     [DbContext(typeof(AppointmentSchedulerDbContext))]
-    partial class AppointmentSchedulerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250207004938_ApplyPendingChanges")]
+    partial class ApplyPendingChanges
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
